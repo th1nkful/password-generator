8907ba93182195799c6c0480db02c3e7bd13b848
Joshua Punshon <punshon.jm@live.com.au>
2020-01-03 22:54:03 +1000

    added readme

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Clipboard } from 'react-native';
 
 export default function App() {
+  const setClipboard = async () => {
+    await Clipboard.setString('sicko mode!');
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Copy" onPress={setClipboard}>Copyrsrsrs</Button>
     </View>
   );
 }
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # password-generator
-
-git remote add origin https://github.com/punshonjm/password-generator.git
